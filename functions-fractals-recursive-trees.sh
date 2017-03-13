2d0129d72d313cebb4992bc90503681e771b4921
RobertBaron <rbaronmonge@gmail.com>
2017-03-13 01:11:16 -0600

    Almost done, too much hacking, getting tired

--- functions-fractals-recursive-trees.sh
+++ functions-fractals-recursive-trees.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-rows=63
-cols=100
+_ROWS=63
+_COLS=100
 
-iteration=1
-
+iterations=$1
+dots=""
 declare -a grid
     # Check argument supplied
 #    if [ $# -eq 0 ] then
@@ -20,35 +20,73 @@ function getIndex {
  echo "$myresult"
 }
 
-function addArt {
-    iteration=1
-    size=16
-    while [ $iteration -le 1 ]
+function addDots {
+   for var in "$@"
     do
+        grid[var]=1
+    done
+}
 
-        x=$rows
-        while [ $x -gt $((rows - size)) ]
-        do
-           y=$((cols / (iteration + 1)))
-           # x
+function addArt {
+    size=$1
+    center=$2
+    startingRow=$3
+    aux=$startingRow
+    dotsParam=$4
 
-           grid[$x,$y]="1"
-           x=$((x-1))
-        done
+    while [ $startingRow -gt $((aux - size)) ]
+    do
+       index=$(getIndex startingRow center)
+       centerDots="$centerDots $index "
+       startingRow=$((startingRow-1))
+    done
+    aux=$startingRow
+    read right rightIndexes < <(getLegIndexes $startingRow $center $size 0)
+    read left leftIndexes < <(getLegIndexes $startingRow $center $size)
 
+    dots="$centerDots $rightIndexes $leftIndexes $dotsParam "
+    subDotsR=""
+    subDotsL=""
+    if [ $size -gt 0 ]; then
+        nextRow=$((startingRow-(size)))
         size=$((size/2))
-        iteration=$((iteration+1))
+
+        read subDotsR < <(addArt $size $right $nextRow $dots)
+        read subDotsL < <(addArt $size $left $nextRow $dots)
+    fi
+    echo "$dots $subDotsR $subDotsL "
+}
+
+function getLegIndexes {
+    row=$1
+    col=$2
+    size=$3
+    direction=$4
+
+    aux=$row
+    while [ $row -gt $((aux - size)) ]
+    do
+       if [  -z ${direction} ] ; then
+        col=$((col-1))
+       else
+        col=$((col+1))
+       fi
+       index=$(getIndex row col)
+       #grid[index]=$iteration
+       indexes="$indexes $index "
+       row=$((row-1))
     done
 
+    echo "$col $indexes"
 }
 
 function initGrid {
-    x=$rows
+    x=$_ROWS
     while [ $x -ge 0 ]
     do
        # y
        i=0
-       y=$cols
+       y=$_COLS
        while [ $y -gt 0 ]
        do
            index=$(getIndex x y)
@@ -62,25 +100,38 @@ function initGrid {
 }
 
 function printGrid {
-    x=$rows
+    x=$_ROWS
+    x2=0
+    result=[]
     while [ $x -ge 0 ]
     do
         # y
-        y=$cols
+        y=$_COLS
+        row=""
         while [ $y -gt 0 ]
         do
             index=$(getIndex x y)
-            echo -n ${grid[index]}
+            row="${row}${grid[index]}"
             y=$((y-1))
         done
         # x
-        echo "$x"
+        result[x2]=$row
+        x=$((x-1))
+        x2=$((x2+1))
+    done
+
+    x=$_ROWS
+    while [ $x -ge 0 ]
+    do
+        echo ${result[x]}
         x=$((x-1))
     done
 }
 
 initGrid
+read res < <(addArt 16 $((_COLS/2)) $_ROWS $dots)
 
-#addArt
+addDots $res
+
 printGrid
 
